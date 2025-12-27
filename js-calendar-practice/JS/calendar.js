d33cf9fa0a8fc7339be517176f0927eb5e25ebc1
Gushiken Jota <j13119613@gmail.com>
2025-12-27 21:48:22 +0900

    refactor: コードレビュー対応

--- js-calendar-practice/JS/calendar.js
+++ js-calendar-practice/JS/calendar.js
@@ -5,27 +5,23 @@ const year = now.getFullYear();
 let month = now.getMonth();
 
 // process.argvを調べていく
-for (let i = 0; i < process.argv.length; i++) {
-  if (process.argv[i] === "-m") {
-    // 1. -m の「次」の要素を取得する
-    const monthInput = process.argv[i + 1];
-    // ステップ2: 文字から数字に変換
-    const monthNum = parseInt(monthInput);
-    // 3. その値が 1〜12 の範囲内かチェックする
-    if (monthNum >= 1 && monthNum <= 12) {
-      month = monthNum - 1;
-    } else {
-      console.error(
-        `エラー: ${monthNum}は正しい月ではありません (1〜12を指定してください)`
-      );
-      process.exit(1);
-    }
+const [_, __, keyword, argMonth] = process.argv;
+// argMonth が存在する場合のみ処理
+if (argMonth) {
+  const monthNum = parseInt(argMonth);
+  if (monthNum >= 1 && monthNum <= 12) {
+    month = monthNum - 1;
+  } else {
+    console.error(
+      `エラー: ${monthNum}は正しい月ではありません (1〜12を指定してください)`
+    );
+    process.exit(1);
   }
 }
 
 // 1. 1日の曜日インデックスの取得
-const firstDayofMonth = new Date(year, month, 1);
-const firstDayofweek = firstDayofMonth.getDay();
+const firstDayOfMonth = new Date(year, month, 1);
+const firstDayOfWeek = firstDayOfMonth.getDay();
 
 // 2. 月の末日の取得 (翌月の0日テクニック)
 const lastDayOfMonth = new Date(year, month + 1, 0);
@@ -34,20 +30,12 @@ const totalDays = lastDayOfMonth.getDate();
 console.log(`      ${month + 1}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-// カレンダーのマス目の通し番号
-let dayCounter = 0;
+let dayCounter = firstDayOfWeek;
 // A. 1日までの空白を描画
-for (let i = 0; i < firstDayofweek; i++) {
-  process.stdout.write("   ");
-  dayCounter++;
-}
+process.stdout.write("   ".repeat(firstDayOfWeek));
 // B. 1日目から31日までを出力
 for (let i = 1; i <= totalDays; i++) {
-  if (i < 10) {
-    process.stdout.write(" " + i + " ");
-  } else {
-    process.stdout.write(i + " ");
-  }
+  process.stdout.write(String(i).padStart(2, " ") + " ");
   dayCounter++;
   if (dayCounter % 7 === 0) {
     console.log("");
